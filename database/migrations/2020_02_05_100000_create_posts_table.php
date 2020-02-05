6012a7744634af48b495769d3448641a4077c340
JustinKoenig123 <48588179+JustinKoenig123@users.noreply.github.com>
2020-02-05 10:20:19 +0100

    Create Database Migrations
    to create a simple Setup to create some Tables. Uncomplete... we have to do some changes while creating our WebApplication.

---
+++ database/migrations/2020_02_05_100000_create_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+          $table->bigIncrements('id');
+          $table->string('title');
+          $table->string('content');
+          $table->string('filename');
+          $table->integer('userid');
+          $table->string('experience');
+          $table->rememberToken();
+          $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
