366ada10be6595d3a0f3b550b3c6b68df3a0a974
JustinKoenig123 <48588179+JustinKoenig123@users.noreply.github.com>
2020-02-05 14:49:00 +0100

    New Table Lessons

---
+++ database/migrations/2020_02_05_100000_create_lessons_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLessonsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lessons', function (Blueprint $table) {
+          $table->bigIncrements('id');
+          $table->string('lessonname');
+          $table->string('professorname');
+          $table->integer('moduleid');
+          $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lessons');
+    }
+}
