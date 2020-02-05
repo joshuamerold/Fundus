b2eec5207ddee6859369c8b5ba5aa159d86b0b36
JustinKoenig123 <48588179+JustinKoenig123@users.noreply.github.com>
2020-02-05 14:49:56 +0100

    updating DatabaseSeeder to Seed some new Database Entries

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        App\User::create([
+          'username' =>'admin',
+          'email' => 'admin@admin.de',
+          'password' => Hash::make('admin'),
+          'courseid' => 1
+        ]);
+
+        App\Course::create([
+          'representativeid' =>1,
+          'secondrepresentativeid' => 1,
+          'name' => 'ON18'
+
+        ]);
+
+        App\Module::create([
+          'name' => 'WebTech 1',
+          'courseid' => '1'
+        ]);
+        App\Module::create([
+          'name' =>'WebUsability1',
+          'courseid' => '1'
+        ]);
     }
 }
