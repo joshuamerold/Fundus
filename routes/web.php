0aac978d636d980276257fb653bf311d80d490c1
JustinKoenig123 <48588179+JustinKoenig123@users.noreply.github.com>
2020-02-05 15:44:24 +0100

    Updating Routes to COnnect with MainController

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,4 @@ Route::get('/test', 'TestController@test');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'MainController@showContent')->middleware('auth');
