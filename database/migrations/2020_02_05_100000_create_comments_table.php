e061715641fd46d13263b682fcf94f97859fd44e
JustinKoenig123 <48588179+JustinKoenig123@users.noreply.github.com>
2020-02-05 14:49:27 +0100

    Changes on logical Implements

--- database/migrations/2020_02_05_100000_create_comments_table.php
+++ database/migrations/2020_02_05_100000_create_comments_table.php
@@ -16,7 +16,7 @@ class CreateCommentsTable extends Migration
         Schema::create('comments', function (Blueprint $table) {
           $table->bigIncrements('id');
           $table->string('content');
-          $table->string('userid');
+          $table->integer('userid');
           $table->timestamps();
         });
     }
