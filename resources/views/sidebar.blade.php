6ac75b3a98d43f33bd4519373c60cc9a38d429d3
fredfies <ries.home@gmail.com>
2020-02-28 23:08:14 +0100

    Minor styling changes

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -1,40 +1,67 @@
 <!-- Sidebar Haupt-Container -->
-  <aside class="main-sidebar">
-    <a href="/home" class="brand-link">
-      <img src="#" alt="" class="brand-image">
-      <span class="brand-text">Fundus</span>
-    </a>
-    <!-- Sidebar -->
-    <div class="sidebar">
-      <!-- Sidebar User Panel -->
-      <div class="user-panel mt-3 pb-3 mb--3 d-flex">
-        <div class="image">
-          <img src="https://i.pinimg.com/236x/8b/33/47/8b3347691677254b345de63fe82f8ef6--batman.jpg" class="img-circle" alt="Profilbild">
-        </div>
-        <div class="info">
-          <a href="#" class="d-block">{{$user->username}}</a>
+<aside class="main-sidebar">
+<!-- Sidebar -->
+  <div class="sidebar">
+    <!-- Sidebar User Panel -->
+    <div class="user-panel mt-3 pb-3 mb--3 d-flex">
+      <div class="image">
+        <img src="https://i.pinimg.com/236x/8b/33/47/8b3347691677254b345de63fe82f8ef6--batman.jpg" class="img-circle" alt="Profilbild">
+      </div>
+
+      <div class="info">
+        <a href="/profile/{{$user->username}}" class="profile-link">{{$user->username}}</a>
+        <!-- LogoutDropdown -->
+        <div class="logout">
+          @guest
+            <li class="nav-item">
+              <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+            </li>
+            @if (Route::has('register'))
+              <li class="nav-item">
+                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+              </li>
+            @endif
+            @else
+            <div class="dropdown">
+              <a id="LogoutDropdown" class="dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                {{ Auth::user()->name }} <span class="caret"></span>
+              </a>
+              <div class="dropdown-menu" aria-labelledby="LogoutDropdown">
+                <a class="dropdown-item" href="{{ route('logout') }}"                    onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+                  {{ __('Logout') }}
+                </a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                  @csrf
+                </form>
+              </div>
+            </div>
+          @endguest
+          </div>
           <span>{{$course->name}}</span>
         </div>
       </div>
-      <!-- Sidebar Menü -->
-      <nav class="mt-2">
-        <ul class="nav nav-pills nav-sidebar flex-column">
-          <li class="nav-item">
-            Aktuelle Fristen
-
-                <table>
-                  <tr>
-                    <td>
-                      WebDesign
-                    </td>
-                    <td>
-                      06.03.2020
-                    </td>
-                  </tr>
-                </table>
-
-          </li>
-        </ul>
-      </nav>
-    </div>
-  </aside>
+  <hr>
+  <!-- Sidebar Menü -->
+  <nav class="mt-2">
+    <ul class="nav nav-pills nav-sidebar flex-column">
+      <li class="nav-item">
+        <!-- Sidebar Karten -->
+        <div class="sidebar-card">
+          <b>Aktuelle Fristen</b><br>
+          <hr class="hr-side">
+          <table>
+            <tr>
+              <td>
+                WebDesign
+              </td>
+              <td class="date">
+                06.03.2020
+              </td>
+            </tr>
+          </table>
+        </div>
+      </li>
+    </ul>
+  </nav>
+</div>
+</aside>
