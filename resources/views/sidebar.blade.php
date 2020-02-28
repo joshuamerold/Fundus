abdc474902268129deada1a58660e39d10823345
fredfies <ries.home@gmail.com>
2020-02-28 19:50:24 +0100

    Add sidebar

---
+++ resources/views/sidebar.blade.php
@@ -0,0 +1,40 @@
+<!-- Sidebar Haupt-Container -->
+  <aside class="main-sidebar">
+    <a href="/home" class="brand-link">
+      <img src="#" alt="" class="brand-image">
+      <span class="brand-text">Fundus</span>
+    </a>
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <!-- Sidebar User Panel -->
+      <div class="user-panel mt-3 pb-3 mb--3 d-flex">
+        <div class="image">
+          <img src="https://i.pinimg.com/236x/8b/33/47/8b3347691677254b345de63fe82f8ef6--batman.jpg" class="img-circle" alt="Profilbild">
+        </div>
+        <div class="info">
+          <a href="#" class="d-block">{{$user->username}}</a>
+          <span>{{$course->name}}</span>
+        </div>
+      </div>
+      <!-- Sidebar Menü -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column">
+          <li class="nav-item">
+            Aktuelle Fristen
+
+                <table>
+                  <tr>
+                    <td>
+                      WebDesign
+                    </td>
+                    <td>
+                      06.03.2020
+                    </td>
+                  </tr>
+                </table>
+
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </aside>
