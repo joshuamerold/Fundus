f30f1001a5c18d0991ed71d9b4d0918641c05fdf
fredfies <ries.home@gmail.com>
2020-02-28 23:07:15 +0100

    Add topbar

---
+++ resources/views/topbar.blade.php
@@ -0,0 +1,8 @@
+<div class="topbar-container">
+  <nav class="navbar navbar-light bg-light">
+    <a class="navbar-brand" href="#">
+      <img src="../assets/Add_Plus.svg" width="30" height="30" class="d-inline-block align-top" alt="Fundus Logo">
+      Fundus
+    </a>
+  </nav>
+</div>
