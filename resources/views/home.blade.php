ecf9f3efd1f34f1d3c2edd163fa88c788481229e
fredfies <ries.home@gmail.com>
2020-02-29 12:27:33 +0100

    Add module card edit button with dropdown menu

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,25 +1,4 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Fundus') }}</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-</head>
-
+@include('standards/head')
 
 <body class="home-body">
 
@@ -32,9 +11,9 @@
         <div class="col-md-8">
             <div>
               <div>
-                <button class="button-add-lessons" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><span class="credits"></span>
+                <button class="fa fa-caret-down button-add-lessons" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><span class="credits"></span>
                 </button>
-                <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+                <div class="dropdown-menu fa-caret-down" aria-labelledby="dropdownMenu2">
                   <li><a href="/add/module">Modul hinzufügen</a></li>
 		              <li><a href="/add/lesson">Lehrveranstaltung hinzufügen</a></li>
                 </div>
@@ -46,12 +25,23 @@
 
                 @foreach($modules as $module)
 
-                <div class="mt-3 module-card col-md-5">
-                    <b>{{$module->name}}</b><br>
+                <div class="mt-3 module-card col-md-6">
+                  <div class="row">
+                    <div class="col-md-11"><b>{{$module->name}}</b><br>
+                    </div>
+                    <div class="justify-content-end dropdown">
+                      <i class="fa fa-caret-down"></i>
+                      <div class="dropdown-content">
+                        <a href="#">Modul bearbeiten</a>
+                        <a href="#">Modul löschen</a>
+                      </div>
+                    </div>                    
+                  </div>
                     <hr>
                     @foreach($lessons as $lesson)
                       @if($lesson->moduleid == $module->id)
-                        <a href="/{{$lesson->id}}/show/zusammenfassung">{{$lesson->lessonname}}</a>, {{$lesson->professorname}}<br>
+                        <a href="/{{$lesson->id}}/show/zusammenfassung">{{$lesson->lessonname}}</a><br>
+                        <p>{{$lesson->professorname}}</p>
                       @endif
                     @endforeach
 
