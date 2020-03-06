dc727234737861c441e5d95b46bd567750572299
fredfies <ries.home@gmail.com>
2020-03-06 21:24:10 +0100

    Change table look

--- resources/views/lessons/allCategories.blade.php
+++ resources/views/lessons/allCategories.blade.php
@@ -1,135 +1,164 @@
-<!-- Zusammenfassung-Part -->
-<a href="/{{$currentLesson->id}}/add/File" class="button-add-file">FIle hinzufügen</a>
-<table>
-  <thead>
-    <th>Typ</th>
-    <th>Dateiname</th>
-    <th>Dozent</th>
-    <th>Jahrgang</th>
-    <th>Datum</th>
-    <th>Hochgeladen von</th>
-    <th>Bewertung</th>
-    <th>Action</th>
-  </thead>
+@include('standards/head')
+@include('sidebar')
+@include('topbar')
+<script>
+$(document).ready(function () {
+$('#dtBasicExample').DataTable();
+$('.dataTables_length').addClass('bs-select');
+});
+</script>
+
+<body class="allCategories-body">
+  <a href="/{{$currentLesson->id}}/add/File" class="button-add-file"></a>
+  <table id="table-zusammenfassung" class="table table-striped table-bordered table-sm" cellspacing="0" width="100%">
+
+    <thead>
+      <th class="th-sm">Typ</th>
+      <th class="th-sm">Dateiname</th>
+      <th class="th-sm">Dozent</th>
+      <th class="th-sm">Jahrgang</th>
+      <th class="th-sm">Datum</th>
+      <th class="th-sm">Hochgeladen von</th>
+      <th class="th-sm">Bewertung</th>
+      <th class="th-sm">Action</th>
+    </thead>
+
+    @foreach($files as $file)
+    <!-- Zusammenfassung-Part -->
+    @if($file->type == "zusammenfassung")
+    <tbody>
+      <td>{{$file->extension}}</td>
+      <td>{{$file->name}}</td>
+      <td>{{$currentLesson->professorname}}</td>
+      <td>
+          @foreach($allUsers as $tempUser)
+            @if($file->creatoruserid == $tempUser->id)
+              @foreach($courses as $course)
+                @if($tempUser->courseid == $course->id)
+                  {{$course->name}}
+                @endif
+              @endforeach
+            @endif
+          @endforeach
+      </td>
+      <td>{{$file->updated_at}}</td>
+      <td>
+          @foreach($creators as $creator)
+            @if($file->creatoruserid == $creator->id)
+              {{$creator->username}}
+            @endif
+          @endforeach
+      </td>
+      <td>{{$file->voting}}</td>
+      <td>
+        <!-- {{Request::path()}} -->
+        <table class="inner-table">
+          <tr>
+            <td><a href="/download/{{$file->id}}" class="fa fa-arrow-circle-o-down"> </a></td>
+            <td><a href="/{{$file->id}}/add/comment" class="fa fa-comment-o" aria-hidden="true"> </a></td>
+            <td>
+            @if($file->creatoruserid == $user->id)
+            <a href="/zusammenfassung/{{$currentLesson->id}}/{{$file->id}}/delete" class="fa fa-trash"> </a>
+            @endif
+            </td>
+          </tr>
+        </table>
+      </td>
+    </tbody>
+    @endif
+    <!--Altklausuren-Part -->
+    @if($file->type == "altklausur")
+    <tbody>
+      <td>{{$file->extension}}</td>
+      <td>{{$file->name}}</td>
+      <td>{{$currentLesson->professorname}}</td>
+      <td>
+          @foreach($allUsers as $tempUser)
+            @if($file->creatoruserid == $tempUser->id)
+              @foreach($courses as $course)
+                @if($tempUser->courseid == $course->id)
+                  {{$course->name}}
+                @endif
+              @endforeach
+            @endif
+          @endforeach
+      </td>
+      <td>{{$file->updated_at}}</td>
+      <td>
+          @foreach($creators as $creator)
+            @if($file->creatoruserid == $creator->id)
+              {{$creator->username}}
+            @endif
+          @endforeach
+      </td>
+      <td>{{$file->voting}}</td>
+      <td>
+        <!-- {{Request::path()}} -->
+        <table class="inner-table">
+          <tr>
+            <td><a href="/download/{{$file->id}}" class="fa fa-arrow-circle-o-down"> </a></td>
+            <td><a href="/{{$file->id}}/add/comment" class="fa fa-comment-o" aria-hidden="true"> </a></td>
+            <td>
+            @if($file->creatoruserid == $user->id)
+            <a href="/zusammenfassung/{{$currentLesson->id}}/{{$file->id}}/delete" class="fa fa-trash"> </a>
+            @endif
+            </td>
+          </tr>
+        </table>
+      </td>
+    </tbody>
+    @endif
+    <!-- Karteikarten-Part -->
+    @if($file->type == "karteikarte")
+    <tbody>
+      <td>{{$file->extension}}</td>
+      <td>{{$file->name}}</td>
+      <td>{{$currentLesson->professorname}}</td>
+      <td>
+          @foreach($allUsers as $tempUser)
+            @if($file->creatoruserid == $tempUser->id)
+              @foreach($courses as $course)
+                @if($tempUser->courseid == $course->id)
+                  {{$course->name}}
+                @endif
+              @endforeach
+            @endif
+          @endforeach
+      </td>
+      <td>{{$file->updated_at}}</td>
+      <td>
+          @foreach($creators as $creator)
+            @if($file->creatoruserid == $creator->id)
+              {{$creator->username}}
+            @endif
+          @endforeach
+      </td>
+      <td>{{$file->voting}}</td>
+      <td>
+        <!-- {{Request::path()}} -->
+        <table class="inner-table">
+          <tr>
+            <td><a href="/download/{{$file->id}}" class="fa fa-arrow-circle-o-down"> </a></td>
+            <td><a href="/{{$file->id}}/add/comment" class="fa fa-comment-o" aria-hidden="true"> </a></td>
+            <td>
+            @if($file->creatoruserid == $user->id)
+            <a href="/zusammenfassung/{{$currentLesson->id}}/{{$file->id}}/delete" class="fa fa-trash"> </a>
+            @endif
+            </td>
+          </tr>
+        </table>
+      </td>
+    </tbody>
+    @endif
+    @endforeach
+  </table>
 
 
-  @foreach($files as $file)
+<!--TABS ab hier -->
 
 
-  <!-- Zusammenfassung-Part -->
-  @if($file->type == "zusammenfassung")
-  <tbody>
-    <td>{{$file->extension}}</td>
-    <td>{{$file->name}}</td>
-    <td>{{$currentLesson->professorname}}</td>
-    <td>
-        @foreach($allUsers as $tempUser)
-          @if($file->creatoruserid == $tempUser->id)
-            @foreach($courses as $course)
-              @if($tempUser->courseid == $course->id)
-                {{$course->name}}
-              @endif
-            @endforeach
-          @endif
-        @endforeach
-    </td>
-    <td>{{$file->updated_at}}</td>
-    <td>
-        @foreach($creators as $creator)
-          @if($file->creatoruserid == $creator->id)
-            {{$creator->username}}
-          @endif
-        @endforeach
-    </td>
-    <td>{{$file->voting}}</td>
-    <td>
-      <!-- {{Request::path()}} -->
-      <a href="/download/{{$file->id}}">Download</a>
-      <a href="/{{$file->id}}/add/comment">kommentieren</a>
-      @if($file->creatoruserid == $user->id)
-      <a href="/zusammenfassung/{{$currentLesson->id}}/{{$file->id}}/delete">Löschen</a>
-      @endif
-
-    </td>
-  </tbody>
-  @endif
 
 
-  <!--Altklausuren-Part -->
-  @if($file->type == "altklausur")
-  <tbody>
-    <td>{{$file->extension}}</td>
-    <td>{{$file->name}}</td>
-    <td>{{$currentLesson->professorname}}</td>
-    <td>
-        @foreach($allUsers as $tempUser)
-          @if($file->creatoruserid == $tempUser->id)
-            @foreach($courses as $course)
-              @if($tempUser->courseid == $course->id)
-                {{$course->name}}
-              @endif
-            @endforeach
-          @endif
-        @endforeach
-    </td>
-    <td>{{$file->updated_at}}</td>
-    <td>
-        @foreach($creators as $creator)
-          @if($file->creatoruserid == $creator->id)
-            {{$creator->username}}
-          @endif
-        @endforeach
-    </td>
-    <td>{{$file->voting}}</td>
-    <td>
-      <!-- {{Request::path()}} -->
-      <a href="/download/{{$file->id}}">Download</a>
-      <a href="/{{$file->id}}/add/comment">kommentieren</a>
-      @if($file->creatoruserid == $user->id)
-      <a href="/altklausur/{{$currentLesson->id}}/{{$file->id}}/delete">Löschen</a>
-      @endif
-
-    </td>
-  </tbody>
-  @endif
 
 
-  <!-- Karteikarten-Part -->
-  @if($file->type == "karteikarte")
-  <tbody>
-    <td>{{$file->extension}}</td>
-    <td>{{$file->name}}</td>
-    <td>{{$currentLesson->professorname}}</td>
-    <td>
-        @foreach($allUsers as $tempUser)
-          @if($file->creatoruserid == $tempUser->id)
-            @foreach($courses as $course)
-              @if($tempUser->courseid == $course->id)
-                {{$course->name}}
-              @endif
-            @endforeach
-          @endif
-        @endforeach
-    </td>
-    <td>{{$file->updated_at}}</td>
-    <td>
-        @foreach($creators as $creator)
-          @if($file->creatoruserid == $creator->id)
-            {{$creator->username}}
-          @endif
-        @endforeach
-    </td>
-    <td>{{$file->voting}}</td>
-    <td>
-      <!-- {{Request::path()}} -->
-      <a href="/download/{{$file->id}}">Download</a>
-      <a href="/{{$file->id}}/add/comment">kommentieren</a>
-      @if($file->creatoruserid == $user->id)
-      <a href="/karteikarte/{{$currentLesson->id}}/{{$file->id}}/delete">Löschen</a>
-      @endif
-
-    </td>
-  </tbody>
-  @endif
-  @endforeach
-</table>
+</body>
