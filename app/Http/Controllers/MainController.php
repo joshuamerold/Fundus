0dbca0f2ce86bb713056f08e95d7efca60c88293
JustinKoenig123 <48588179+JustinKoenig123@users.noreply.github.com>
2020-03-19 15:35:30 +0100

    Handle for Userrightsystem

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -7,17 +7,40 @@ use Illuminate\Support\Facades\Auth;
 
 use App\User;
 use App\Course;
+use App\CourseRight;
 use App\Module;
 use App\Lesson;
 use App\File;
 
 class MainController extends NavbarController
 {
+
+    public function add(Request $request, $coursename){
+        $course = CourseRight::all()->where('coursepartition', $coursename)->first();
+
+        $course->representative1 = $request->adminuser1;
+        $course->representative2 = $request->adminuser2;
+        $course->save();
+
+        $adminuser1 = User::all()->where('id', $request->adminuser1)->first();
+        $adminuser1->rights = "admin";
+        $adminuser1->save();
+
+        $adminuser2 = User::all()->where('id', $request->adminuser2)->first();
+        $adminuser2->rights = "admin";
+        $adminuser2->save();
+
+        return redirect('/home')->with('success', 'Sie haben erfolgreich die Kursesprecher bestimmt!');
+    }
+
+
+
     public function showContent(){
       $user = Auth::user();
       $userid = $user->id;
       $courseid = $user->courseid;
 
+      $allCourseUsers = User::all()->where('coursename', $user->coursename);
       $currentCourse = Course::all()->where('id', $courseid)->first();
       $currentCourseName = $currentCourse->studycoursename;
 
@@ -25,7 +48,15 @@ class MainController extends NavbarController
       $lessons = Lesson::all();
       $files = File::all();
 
-      return view('home')->with('user',$user)->with('course', $currentCourse)->with('modules', $modules)->with('lessons', $lessons)->with('files', $files);
+      $valCourserights = CourseRight::all()->where('coursepartition', $user->coursename)->first();
+
+      $boolRepresentitive = true;
+
+      if($valCourserights->representative1=='none' && $valCourserights->representative2=='none'){
+        $boolRepresentitive = false;
+      }
+
+      return view('home')->with('user',$user)->with('course', $currentCourse)->with('modules', $modules)->with('lessons', $lessons)->with('files', $files)->with('alreadyAdmins', $boolRepresentitive)->with('allCourseUsers', $allCourseUsers);
       // $modules = Modules::all()->where('')
       // $users = User::all();
       // return view('test')->with('users', $users);
