7aa6cb386a651527f12615a94e99507e662d023d
JustinKoenig123 <48588179+JustinKoenig123@users.noreply.github.com>
2020-03-30 19:20:38 +0200

    Adding some "specials" :-)

---
+++ resources/views/happyEaster.blade.php
@@ -0,0 +1,13 @@
+@include('standards/head')
+@include('sidebar')
+@include('topbar')
+@include('inc/messages')
+<body class="editModule-body">
+  <div class="offset-md-2 col-md-8">
+    <div class="" style="background-color:none;">
+      <video src="/films/happyeaster.mp4" autoplay controls style="margin-top:10%; width:700px">
+        Frohe Ostern!
+      </video>
+    </div>
+  </div>
+</body>
