6e4900dae6c4154ed4f89db849856214941314cc
JanaHalter <48588088+JanaHalter@users.noreply.github.com>
2020-03-30 13:51:51 +0200

    Update addDate.blade.php
    Anpassung des Stylings der AddDate.blade.php

--- resources/views/forms/addDate.blade.php
+++ resources/views/forms/addDate.blade.php
@@ -3,18 +3,37 @@
 @include('topbar')
 @include('inc/messages')
 
+
+
+
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.5.0/js/bootstrap-datepicker.js"></script>
 
 <body class="addDate-body">
-<div class="container">
-  <h1>Neuen Termin erstellen</h1>
+  <div class="card offset-md-2 col-md-8">
+  <h2 class="mt-3">Neuen Termin erstellen</h2>
+  <p></p>
+  <div class="input-group mb-3">
+  <div class="input-group-prepend">
+  <label class="input-group-text" for="inputGroupSelect01">Hier das Datum auswählen</label>
+</div>
   <form class="" action="/add/date/create" method="post">
     @csrf
-    <input class="date form-control" type="text" name="date">
-    <input type="text" name="name" value="neuer Terminname">
-    <input type="submit" name="" value="senden">
+    <input class="date form-control" type="text" name="date" value="Datum">
+  </div>
+
+  <div class="input-group mb-3">
+  <div class="input-group-prepend">
+    <label class="input-group-text" for="inputGroupSelect01">Terminname erstellen</label>
+  </div>
+    <input type="text" name="name" value="neuer Terminname" class="form-control">
+  </div>
+
+
+    <div class="form-group row justify-content-center">
+        <input type="submit" name="" value="senden" class="btn btn-red">
+    </div>
   </form>
   <div class="form-group row justify-content-center">
     <a href="/"<button type="button" name="button">zurück</button>
