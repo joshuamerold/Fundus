cb96e4ec7703ecc9b319e754916011b5e8fe0490
fredfies <ries.home@gmail.com>
2020-03-30 17:36:08 +0200

    Set correct coding spaces

--- resources/views/forms/addDate.blade.php
+++ resources/views/forms/addDate.blade.php
@@ -7,33 +7,33 @@
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.5.0/js/bootstrap-datepicker.js"></script>
 
-<body class="addDate-body">
-<div class="container">
-  <h1>Neuen Termin erstellen</h1>
-  <form class="" action="/add/date/create" method="post">
-    @csrf
-    <input class="date form-control" type="text" name="date" required>
-    <input type="text" name="name" value="neuer Terminname" required>
-    <input type="submit" name="" value="senden">
-  </form>
-  <div class="form-group row justify-content-center">
-    <a href="/"<button type="button" name="button">zurück</button>
-  </div>
-</div>
-<script type="text/javascript">
-var date = new Date();
-date.setDate(date.getDate());
+  <body class="addDate-body">
+    <div class="container">
+      <h1>Neuen Termin erstellen</h1>
+      <form class="" action="/add/date/create" method="post">
+        @csrf
+        <input class="date form-control" type="text" name="date" required>
+        <input type="text" name="name" value="neuer Terminname" required>
+        <input type="submit" name="" value="senden">
+      </form>
+      <div class="form-group row justify-content-center">
+        <a href="/"<button type="button" name="button">zurück</button>
+      </div>
+    </div>
+    <script type="text/javascript">
+    var date = new Date();
+    date.setDate(date.getDate());
 
-    $('.date').datepicker({
-        startDate: date,
-        locale: 'de',
-       format: 'dd.mm.yyyy'
+        $('.date').datepicker({
+            startDate: date,
+            locale: 'de',
+           format: 'dd.mm.yyyy'
 
-     });
+         });
 
-</script>
+    </script>
 
 
-</body>
+  </body>
 
 </html>
