2b3bc0ac406047d64ce743acc44a28ca0dc95b78
JanaHalter <48588088+JanaHalter@users.noreply.github.com>
2020-03-30 14:03:36 +0200

    Update addComment.blade.php
    Update addComment.blade

--- resources/views/forms/addComment.blade.php
+++ resources/views/forms/addComment.blade.php
@@ -1,18 +1,28 @@
 @include('inc.messages')
-<h1>Comments hier</h1>
+@include('standards/head')
+@include('sidebar')
+@include('topbar')
+
+<body class="addComment-body">
+  <div class="card offset-md-2 col-md-8">
+<h2 class="mt-3">Comments hier</h2>
 @if($fileToShow->extension != 'pdf' && $fileToShow->extension != 'txt')
   <p>keine Vorschau</p>
 @else
 <iframe src="http://fundus.localhost/.{{$fileToShow->path}}" frameborder="0" height="500px" width="500px"></iframe>
 @endif
+</div>
 
-<h2>Votes für die File lol</h2>
+  <div class="card offset-md-2 col-md-8">
+<h2 class="mt-3">Votes für die File lol</h2>
 {{$rating}} <br>
 
 <a href="/{{$lessonId}}/{{$fileToShow->id}}/add/comment/up">hier upvoten</a>
 <a href="/{{$lessonId}}/{{$fileToShow->id}}/add/comment/down">hier downvoten</a>
+</div>
 
-<h2>Comment-Section</h2>
+  <div class="card offset-md-2 col-md-8">
+<h2 class="mt-3">Comment-Section</h2>
 @foreach($comments as $comment)
 <p>
   <div class="">
@@ -29,21 +39,25 @@
         {{$user->username}} <br>
         Kurs des Kommentierers=
         {{$user->coursename}} <br>
-        Inhalt des KOmmentars= {{$comment->content}}
+        Inhalt des Kommentars= {{$comment->content}}
       @endif
     @endforeach
     <br>
   </div>
 </p>
 @endforeach
+</div>
 
-<h2>KOmmentar hinzufügen</h2>
+  <div class="card offset-md-2 col-md-8">
+<h2 class="mt-3">Kommentar hinzufügen</h2>
 <form class="" action="/{{$lessonId}}/{{$fileToShow->id}}/add/comment/store" method="post">
   <input type="text" name="content" value="neuer Comment">
   <input type="submit" name="" value="abschicken">
   @csrf
 </form>
+</div>
 
 <div class="form-group row justify-content-center">
   <a href="/{{$lessonId}}/show"<button type="button" name="button">zurück</button>
 </div>
+</body>
