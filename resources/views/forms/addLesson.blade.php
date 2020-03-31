e47829333a51b07f2c49df06385e553dcd5321ec
JustinKoenig123 <48588179+JustinKoenig123@users.noreply.github.com>
2020-03-31 08:43:46 +0200

    Debugging Inputs

--- resources/views/forms/addLesson.blade.php
+++ resources/views/forms/addLesson.blade.php
@@ -9,62 +9,62 @@
     <p></p>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <form id="lessonForm" class="" action="/{{$semester}}/add/lesson/create/lesson" method="post">
-  @csrf
+      @csrf
 
-<div class="input-group mb-3">
-<div class="input-group-prepend">
-  <label class="input-group-text" for="inputGroupSelect01">Vorlesung</label>
-</div>
-<input type="text" name="lessonname" value="" placeholder="Name der Vorlesung" class="form-control">
-  </div>
+      <div class="input-group mb-3">
+        <div class="input-group-prepend">
+          <label class="input-group-text" for="inputGroupSelect01">Vorlesung</label>
+        </div>
+        <input type="text" name="lessonname" value="" placeholder="Name der Vorlesung" class="form-control">
+      </div>
 
-  <p>
-    <div class="input-group mb-3">
-    <div class="input-group-prepend">
-      <label class="input-group-text" for="inputGroupSelect01">Dozent</label>
-    </div>
-    <select name="sex" class="custom-select" id="sex">
-    <option value="Herr">Herr</option>
-    <option value="Frau">Frau</option>
-    <option value="Divers">Divers</option>
-    </select>
-  <input type="text" name="professorname" value="" placeholder="Name des Dozenten" class="form-control">
-</div>
-<p>
+      <p>
+        <div class="input-group mb-3">
+          <div class="input-group-prepend">
+            <label class="input-group-text" for="inputGroupSelect01">Dozent</label>
+          </div>
+          <select name="sex" class="custom-select" id="sex">
+            <option value="Herr">Herr</option>
+            <option value="Frau">Frau</option>
+            <option value="Divers">Divers</option>
+          </select>
+          <input type="text" name="professorname" value="" placeholder="Name des Dozenten" class="form-control">
+        </div>
+        <p>
 
-<div class="input-group mb-3">
-    <div class="input-group-prepend">
-      <label class="input-group-text" for="inputGroupSelect01">Modulname</label>
-    </div>
-    <select id="module" name="module" class="custom-select">
-        @foreach($modules as $module)
-        <option class="selects" value="{{$module->id}}">{{$module->name}}</option>
-        @endforeach
-    </select>
-    <input type="text" id="moduleInput" name="newModule" style="display:none" value="" placeholder="Modulname"/>
+          <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <label class="input-group-text" for="inputGroupSelect01">Modulname</label>
+            </div>
+            <select id="module" name="module" class="custom-select">
+              @foreach($modules as $module)
+              <option class="selects" value="{{$module->id}}">{{$module->name}}</option>
+              @endforeach
+            </select>
+            <input type="text" id="moduleInput" name="newModule" style="display:none" value="" placeholder="Modulname"/>
 
           </div>
-      </br>
+        </br>
 
-      <br>
-<div class="form-group row justify-content-center">
-    <input  id="addModule" type="submit" name="" value="abschicken" class="btn btn-red">
-</div>
-</form>
+        <br>
+        <div class="form-group row justify-content-center">
+          <input  type="submit" name="" value="abschicken" class="btn btn-red">
+        </div>
+      </form>
 
-<div class="form-group row justify-content-center">
-       <button id="addModule" type="button">neues Modul hinzufügen</button>
-       <script type="text/javascript">
-       $('#addModule').click(function(){
-         $('#moduleInput').css('display','block');
-         $('#lessonForm').attr('action', '/{{$semester}}/add/lesson/create/module')
-         $('#module').remove();
-         $('#addModule').remove();
-       });
-       </script>
-       <div class="form-group row justify-content-center">
-         <a href="/home"<button type="button" name="button">zurück</button>
-       </div>
-</div>
-</div>
-</body>
+      <div class="form-group row justify-content-center">
+        <button id="addModule" type="button">neues Modul hinzufügen</button>
+        <script type="text/javascript">
+        $('#addModule').click(function(){
+          $('#moduleInput').css('display','block');
+          $('#lessonForm').attr('action', '/{{$semester}}/add/lesson/create/module')
+          $('#module').remove();
+          $('#addModule').remove();
+        });
+        </script>
+        <div class="form-group row justify-content-center">
+          <a href="/home"<button type="button" name="button">zurück</button>
+          </div>
+        </div>
+      </div>
+    </body>
