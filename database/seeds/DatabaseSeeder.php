9d60dc962087b6f91bc41fb5f9cc367d16fc51d6
JustinKoenig123 <48588179+JustinKoenig123@users.noreply.github.com>
2020-04-01 08:50:15 +0200

    setted new seeds and Debugging

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,9 +12,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         App\User::create([
-          'username' =>'admin',
+          'username' =>'admin.admin.18',
           'email' => 'admin@admin.de',
           'password' => Hash::make('admin'),
+          'imageURL' => '/profilePictures/chase-jiggiemon-wilson-tu_uBj2_oTQ-unsplash.jpg',
           'courseid' => 1,
           'coursename' => 'ON18'
         ]);
@@ -40,8 +41,15 @@ class DatabaseSeeder extends Seeder
           'coursename' => 'ON16'
         ]);
         App\User::create([
-          'username' =>'holzer',
+          'username' =>'admin1.holzer.20',
           'email' => 'admin@holzer.de',
+          'password' => Hash::make('admin'),
+          'courseid' => 2,
+          'coursename' => 'HH20'
+        ]);
+        App\User::create([
+          'username' =>'admin2.holzer.20',
+          'email' => 'admin2@holzer.de',
           'password' => Hash::make('admin'),
           'courseid' => 2,
           'coursename' => 'HH20'
@@ -74,60 +82,107 @@ class DatabaseSeeder extends Seeder
         ]);
         App\Module::create([
           'id' => 'M$2y$10$q.LpuWRa1ITL2ofXRPYe6ussdsdsdsdssssssfQpVVPdGgYEZhBva',
-          'name' =>'WebUsability1',
+          'name' =>'WebUsability 2',
+          'courseid' => 1,
+          'creatoruserid' => 1,
+          'semester' => 3
+        ]);
+        App\Module::create([
+          'id' => 'M$2y$10$q.LpuWRa1ITL2ofXRPYe6us9XfvfghzdsssssfQpVVPdGgYEZhBva',
+          'name' =>'WebTech 3',
+          'courseid' => 1,
+          'creatoruserid' => 1,
+          'semester' => 3
+        ]);
+        App\Module::create([
+          'id' => 'M$2y$10$q.LpuWRa1ITL2ofXRPYe6us9Xi7Pzdsdkjui8fQpVVPdGgYEZhBva',
+          'name' =>'WebDesign 1',
           'courseid' => 1,
           'creatoruserid' => 1,
           'semester' => 1
         ]);
         App\Module::create([
-          'id' => 'M$2y$10$q.LpuWRa1ITL2ofXRPYe6us9XfvfghzdsssssfQpVVPdGgYEZhBva',
-          'name' =>'WebScience2',
-          'courseid' => 1,
-          'creatoruserid' => 1,
-          'semester' => 2
-        ]);
-        App\Module::create([
-          'id' => 'M$2y$10$q.LpuWRa1ITL2ofXRPYe6us9Xi7Pzdsdkjui8fQpVVPdGgYEZhBva',
-          'name' =>'Webshit1',
-          'courseid' => 1,
-          'creatoruserid' => 1,
-          'semester' => 2
-        ]);
-        App\Module::create([
           'id' => 'M$2y$10$s,dljuwh1ITL2ofXRPYe6us9Xi7PzssssssssfQpVVPdGgYEZhBva',
-          'name' =>'WebMan1',
+          'name' =>'Recht',
           'courseid' => 1,
           'creatoruserid' => 1,
-          'semester' => 5
+          'semester' => 4
         ]);
 
         App\Lesson::create([
           'id' => 'L$2y$10$q.LpuWRa1ITL2ofXRPYe6us9Xi7Pzr4rIIH3pfQpVVPdGgYEZhBva',
-          'lessonname' =>'Datenbanksysteme und Integration',
-          'professorname' => 'Herr Seith',
-          'moduleid' =>  'M$2y$10$q.LpuWRa1ITL2ofXRPYe6us9Xi7PzssssssssfQpVVPdGgYEZhBva',
+          'lessonname' =>'PHP / Webanwendungen mit PHP',
+          'professorname' => 'Herr Albert',
+          'moduleid' =>  'M$2y$10$q.LpuWRa1ITL2ofXRPYe6us9XfvfghzdsssssfQpVVPdGgYEZhBva',
           'creator_userid' =>1
         ]);
         App\Lesson::create([
           'id' => 'L$2y$10$q.LpuWRa1ITL2ofXRPYe6us9Xi7Pzr4rIIH3pfQpVVPdG22222222',
-          'lessonname' =>'Katastrophale Java-Vorlesung',
-          'professorname' => 'Herr Schadt',
-          'moduleid' =>  'M$2y$10$q.LpuWRa1ITL2ofXRPYe6us9Xi7PzssssssssfQpVVPdGgYEZhBva',
+          'lessonname' =>'Bootstrap',
+          'professorname' => 'Herr Dietrich',
+          'moduleid' =>  'M$2y$10$q.LpuWRa1ITL2ofXRPYe6us9XfvfghzdsssssfQpVVPdGgYEZhBva',
           'creator_userid' =>1
         ]);
         App\Lesson::create([
           'id' => 'L$2y$10$q.LpuWRa1ITL2ofXRPYe6us9Xi7Pzr4rIIH3pfQpVVPdGdsfdfggds',
-          'lessonname' =>'Statistik und BlaBla',
-          'professorname' => 'Herr Weird',
+          'lessonname' =>'CSS Preprocessing',
+          'professorname' => 'Herr Dietrich',
+          'moduleid' => 'M$2y$10$q.LpuWRa1ITL2ofXRPYe6us9XfvfghzdsssssfQpVVPdGgYEZhBva',
+          'creator_userid' =>1
+        ]);
+        App\Lesson::create([
+          'id' => 'L$2y$10$q.LpuWRa1ITL2ofXRPYe6us9Xi7Pzr4rIIH3pfQpVVPdGdsfsddfggds',
+          'lessonname' =>'Laravel',
+          'professorname' => 'Herr Schäffer',
+          'moduleid' => 'M$2y$10$q.LpuWRa1ITL2ofXRPYe6us9XfvfghzdsssssfQpVVPdGgYEZhBva',
+          'creator_userid' =>1
+        ]);
+
+        App\Lesson::create([
+          'id' => 'L$2y$10$q.LpuWRa1ITL2ofXRPYe6us9Xi7Pzr4rIIH3pfQpVVPdGdsfsddfggds',
+          'lessonname' =>'Statistik',
+          'professorname' => 'Herr Prof. Dr. Wirth',
+          'moduleid' => 'M$2y$10$q.LpuWRa1ITL2ofXRPYe6ussdsdsdsdssssssfQpVVPdGgYEZhBva',
+          'creator_userid' =>1
+        ]);
+        App\Lesson::create([
+          'id' => 'L$2y$10$q.LpuWRa1ITL2ofXRPYe6us9Xi7Pzr4rIIH3pfdccdddds',
+          'lessonname' =>'HTML/CSS',
+          'professorname' => 'Herr Albert',
+          'moduleid' => 'M$2y$10$q.LpuWRa1ITL2ofXRPYe6us9Xi7PzssssssssfQpVVPdGgYEZhBva',
+          'creator_userid' =>1
+        ]);
+
+        App\Lesson::create([
+          'id' => 'L$2y$10$q.LpusdsdffggffRPYe6us9Xi7Pzr4rIIH3pfQpVVPdGdsfsddfggds',
+          'lessonname' =>'Typografie',
+          'professorname' => 'Herr Grossmann',
+          'moduleid' => 'M$2y$10$q.LpuWRa1ITL2ofXRPYe6us9Xi7Pzdsdkjui8fQpVVPdGgYEZhBva',
+          'creator_userid' =>1
+        ]);
+        App\Lesson::create([
+          'id' => 'L$2y$10$q.LpuWRa1ITL2ofXsdvfghhi7Pzr4rIIH3pfdccdddds',
+          'lessonname' =>'Formen und Linien',
+          'professorname' => 'Herr Starzmann',
           'moduleid' => 'M$2y$10$q.LpuWRa1ITL2ofXRPYe6us9Xi7Pzdsdkjui8fQpVVPdGgYEZhBva',
           'creator_userid' =>1
         ]);
 
+        App\Lesson::create([
+          'id' => 'L$2y$10$q.LpsadsdasasdauWRa1ITL2ofXsdvfghhi7Pzr4rIIH3pfdccdddds',
+          'lessonname' =>'Onlinerecht',
+          'professorname' => 'Frau Dietz-Roth',
+          'moduleid' => 'M$2y$10$s,dljuwh1ITL2ofXRPYe6us9Xi7PzssssssssfQpVVPdGgYEZhBva',
+          'creator_userid' =>1
+        ]);
+
+
+
         App\File::create([
           'id' => 'F$2y$10$q.LpuWRasaTL2ofXRPYe6us9Xi7Pzr4rIIH3pfQpVVPdGgYEZhBva',
-          'name' =>'Web Usability Semester3 zusammendfa.pdf',
+          'name' =>'PHP_Zusammenfassung.pdf',
           'extension' => 'pdf',
-          'path' => '/files/Web Usability Semester3 zusammendfa.pdf',
+          'path' => '/files/PHP_Zusammenfassung.pdf',
           'type' => 'zusammenfassung',
           'lessonid' => 'L$2y$10$q.LpuWRa1ITL2ofXRPYe6us9Xi7Pzr4rIIH3pfQpVVPdGgYEZhBva',
           'creatoruserid' => 1,
@@ -135,27 +190,40 @@ class DatabaseSeeder extends Seeder
           'voting' => 0
         ]);
         App\File::create([
-          'id' => 'F$2y$10$q.LpuWRa1ITL2ofXRPYe6us9Xi7Pzr4rI55555555VPdGgYEZhBva',
-          'name' =>'Ajax.pdf',
+          'id' => 'F$2y$10$q.LpuWRasaTL2ofXRPYe6us9Xi7Pzr4rIIsdsdH3pfQpVVPdGgYEZhBva',
+          'name' =>'superduper_PHP_Zusammenfassung.pdf',
           'extension' => 'pdf',
-          'path' => '/files/Ajax.pdf',
+          'path' => '/files/superduper_PHP_Zusammenfassung.pdf',
           'type' => 'zusammenfassung',
-          'lessonid' => 'L$2y$10$q.LpuWRa1ITL2ofXRPYe6us9Xi7Pzr4rIIH3pfQpVVPdGdsfdfggds',
+          'lessonid' => 'L$2y$10$q.LpuWRa1ITL2ofXRPYe6us9Xi7Pzr4rIIH3pfQpVVPdGgYEZhBva',
           'creatoruserid' => 1,
           'courseid' => 1,
           'voting' => 0
         ]);
         App\File::create([
-          'id' => 'F$2y$10$q.LpuWRa1ITL2ofXRPYe6us9Xi7Pzr4rIIH3pfQpVV11111111Bva',
-          'name' =>'Java.pdf',
+          'id' => 'F$2y$10sddf$q.LpuWRasaTL2ofXRPYe6us9Xi7Pzr4rIIH3pfQpVVPdGgYEZhBva',
+          'name' =>'PHP_albert_alteklausur_ON.pdf',
           'extension' => 'pdf',
-          'path' => '/files/Java.pdf',
+          'path' => '/files/PHP_albert_alteklausur_ON.pdf',
           'type' => 'altklausur',
-          'lessonid' => 'L$2y$10$q.LpuWRa1ITL2ofXRPYe6us9Xi7Pzr4rIIH3pfQpVVPdGdsfdfggds',
+          'lessonid' => 'L$2y$10$q.LpuWRa1ITL2ofXRPYe6us9Xi7Pzr4rIIH3pfQpVVPdGgYEZhBva',
+          'creatoruserid' => 1,
+          'courseid' => 1,
+          'voting' => 0
+        ]);
+        App\File::create([
+          'id' => 'F$2y$10$q.LpuWRa1ITL2ofXRPYe6us9Xi7Pzr4rIIH3pfsdsQpVV11111111Bva',
+          'name' =>'Onlinerecht_Karteikarten.jpg',
+          'extension' => 'jpg',
+          'path' => '/files/Onlinerecht_Karteikarten.jpg',
+          'type' => 'karteikarte',
+          'lessonid' => 'L$2y$10$q.LpsadsdasasdauWRa1ITL2ofXsdvfghhi7Pzr4rIIH3pfdccdddds',
           'creatoruserid' => 2,
           'courseid' => 1,
           'voting' => 0
         ]);
+
+
 
         App\Comment::create([
           'content' =>'Das ist Post NR.1',
@@ -187,8 +255,8 @@ class DatabaseSeeder extends Seeder
 
         App\CourseRight::create([
           'coursepartition' =>'ON18',
-          'representative1' => 'none',
-          'representative2' => 'none'
+          'representative1' => 1,
+          'representative2' => 1
         ]);
 
     }
